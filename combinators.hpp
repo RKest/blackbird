e41077324c190d23ebc79d0f89231ec47f499da3
max <max.ind@spectre.solutions>
2024-08-09 19:17:28 +0200

    Added constexpr

--- combinators.hpp
+++ combinators.hpp
@@ -11,98 +11,98 @@ namespace combinators {
 /////////////////
 
 // I (no bird name)
-auto _id = std::identity{};
+constexpr auto _id = std::identity{};
 
 // B (The Bluebird)
-auto _b = [](auto f, auto g) { return [=](auto x) { return f(g(x)); }; };
+constexpr auto _b = [](auto f, auto g) { return [=](auto x) { return f(g(x)); }; };
 
 // BB (The Bluebird^2)
-auto _bb = [](auto f, auto g, auto h) { return [=](auto x) { return f(g(h(x))); }; };
+constexpr auto _bb = [](auto f, auto g, auto h) { return [=](auto x) { return f(g(h(x))); }; };
 
 // BBB (The Bluebird^3)
-auto _bbb = [](auto f, auto g, auto h, auto i) { return [=](auto x) { return f(g(h(i(x)))); }; };
+constexpr auto _bbb = [](auto f, auto g, auto h, auto i) { return [=](auto x) { return f(g(h(i(x)))); }; };
 
 // B1 (The Blackbird)
-auto _b1 = [](auto f, auto g) { return [=](auto x, auto y) { return f(g(x, y)); }; };
+constexpr auto _b1 = [](auto f, auto g) { return [=](auto x, auto y) { return f(g(x, y)); }; };
 
 // C (The Cardinal) aka `flip` in Haskell
-auto _c = [](auto f) { return [=](auto x, auto y) { return f(y, x); }; };
+constexpr auto _c = [](auto f) { return [=](auto x, auto y) { return f(y, x); }; };
 
 // D2 (The Dovekie)
-auto _d2_ = [](auto f, auto g, auto h) { return [=](auto x, auto y) { return g(f(x), h(y)); }; };
+constexpr auto _d2_ = [](auto f, auto g, auto h) { return [=](auto x, auto y) { return g(f(x), h(y)); }; };
 
 // K (Kestrel)
-auto _l_ = [](auto x, auto y) { return x; };
+constexpr auto _l_ = [](auto x, auto y) { return x; };
 
 // KI
-auto _r_ = [](auto x, auto y) { return y; };
+constexpr auto _r_ = [](auto x, auto y) { return y; };
 
 // Phi (The Phoenix)
-auto _phi = [](auto f, auto g, auto h) { return [=](auto x) { return g(f(x), h(x)); }; };
+constexpr auto _phi = [](auto f, auto g, auto h) { return [=](auto x) { return g(f(x), h(x)); }; };
 
 // Phi1 (The Pheasant)
-auto _phi1_ = [](auto f, auto g, auto h) { return [=](auto x, auto y) { return g(f(x, y), h(x, y)); }; };
+constexpr auto _phi1_ = [](auto f, auto g, auto h) { return [=](auto x, auto y) { return g(f(x, y), h(x, y)); }; };
 
 // Psi (The Psi Bird)
-auto _psi = [](auto f, auto g) { return [=](auto x, auto y) { return f(g(x), g(y)); }; };
+constexpr auto _psi = [](auto f, auto g) { return [=](auto x, auto y) { return f(g(x), g(y)); }; };
 
 // W (The Warbler)
-auto _w = [](auto f) { return [=](auto x) { return f(x, x); }; };
+constexpr auto _w = [](auto f) { return [=](auto x) { return f(x, x); }; };
 
 /////////////////////////////////////////////
 // more convenient binary/unary operations //
 /////////////////////////////////////////////
 
-auto _eq    = [](auto x) { return [x](auto y) { return x == y; }; };
-auto _eq_   = std::equal_to{};
-auto _neq_  = std::not_equal_to{};
-auto _lt    = [](auto x) { return [x](auto y) { return x > y; }; };
-auto lt_    = [](auto x) { return [x](auto y) { return y < x; }; };
-auto _lt_   = std::less{};
-auto _lte_  = std::less_equal{};
-auto _gt_   = std::greater{};
-auto _gte   = [](auto x) { return [x](auto y) { return x >= y; }; };
-auto _gte_  = std::greater_equal{};
-auto _plus  = [](auto x) { return [x](auto y) { return x + y; }; };
-auto _plus_ = std::plus{};
-auto _mul   = [](auto x) { return [x](auto y) { return x * y; }; };
-auto _mul_  = std::multiplies{};
-auto _sub   = [](auto x) { return [x](auto y) { return x - y; }; };
-auto sub_   = [](auto x) { return [x](auto y) { return y - x; }; };
-auto _sub_  = std::minus{};
-auto _mod   = [](auto x) { return [x](auto y) { return x % y; }; };
-auto mod_   = [](auto x) { return [x](auto y) { return y % x; }; };
-auto _mod_  = std::modulus{};
-auto _pow_  = [](auto x, auto y) { return std::pow(x, y); };
-auto _div   = [](auto x) { return [x](auto y) { return x / y; }; };
-auto div_   = [](auto x) { return [x](auto y) { return y / x; }; };
-auto _div_  = std::divides{};
-auto _or_   = std::logical_or{};
-auto _and_  = std::logical_and{};
-auto _neg   = std::negate{};
-auto _not   = std::logical_not{};
-auto _sign  = [](auto x) { return x > 0 ? 1 : x < 0 ? -1 : 0; };
-auto _sqrt  = [](auto x) { return std::sqrt(x); };
-auto _min_  = [](auto x, auto y) { return std::min(x, y); };
-auto _max_  = [](auto x, auto y) { return std::max(x, y); };
+constexpr auto _eq    = [](auto x) { return [x](auto y) { return x == y; }; };
+constexpr auto _eq_   = std::equal_to{};
+constexpr auto _neq_  = std::not_equal_to{};
+constexpr auto _lt    = [](auto x) { return [x](auto y) { return x > y; }; };
+constexpr auto lt_    = [](auto x) { return [x](auto y) { return y < x; }; };
+constexpr auto _lt_   = std::less{};
+constexpr auto _lte_  = std::less_equal{};
+constexpr auto _gt_   = std::greater{};
+constexpr auto _gte   = [](auto x) { return [x](auto y) { return x >= y; }; };
+constexpr auto _gte_  = std::greater_equal{};
+constexpr auto _plus  = [](auto x) { return [x](auto y) { return x + y; }; };
+constexpr auto _plus_ = std::plus{};
+constexpr auto _mul   = [](auto x) { return [x](auto y) { return x * y; }; };
+constexpr auto _mul_  = std::multiplies{};
+constexpr auto _sub   = [](auto x) { return [x](auto y) { return x - y; }; };
+constexpr auto sub_   = [](auto x) { return [x](auto y) { return y - x; }; };
+constexpr auto _sub_  = std::minus{};
+constexpr auto _mod   = [](auto x) { return [x](auto y) { return x % y; }; };
+constexpr auto mod_   = [](auto x) { return [x](auto y) { return y % x; }; };
+constexpr auto _mod_  = std::modulus{};
+constexpr auto _pow_  = [](auto x, auto y) { return std::pow(x, y); };
+constexpr auto _div   = [](auto x) { return [x](auto y) { return x / y; }; };
+constexpr auto div_   = [](auto x) { return [x](auto y) { return y / x; }; };
+constexpr auto _div_  = std::divides{};
+constexpr auto _or_   = std::logical_or{};
+constexpr auto _and_  = std::logical_and{};
+constexpr auto _neg   = std::negate{};
+constexpr auto _not   = std::logical_not{};
+constexpr auto _sign  = [](auto x) { return x > 0 ? 1 : x < 0 ? -1 : 0; };
+constexpr auto _sqrt  = [](auto x) { return std::sqrt(x); };
+constexpr auto _min_  = [](auto x, auto y) { return std::min(x, y); };
+constexpr auto _max_  = [](auto x, auto y) { return std::max(x, y); };
 
 // conversions
-auto _int    = [](auto x) { return static_cast<int>(x); };
-auto _uint   = [](auto x) { return static_cast<unsigned int>(x); };
-auto _bool   = [](auto x) { return static_cast<bool>(x); };
-auto _float  = [](auto x) { return static_cast<float>(x); };
-auto _double = [](auto x) { return static_cast<double>(x); };
+constexpr auto _int    = [](auto x) { return static_cast<int>(x); };
+constexpr auto _uint   = [](auto x) { return static_cast<unsigned int>(x); };
+constexpr auto _bool   = [](auto x) { return static_cast<bool>(x); };
+constexpr auto _float  = [](auto x) { return static_cast<float>(x); };
+constexpr auto _double = [](auto x) { return static_cast<double>(x); };
 
 template <int N>
-auto _nth = [](auto t) { return std::get<N>(t); };
-auto _fst = [](auto t) { return std::get<0>(t); };
-auto _snd = [](auto t) { return std::get<1>(t); };
+constexpr auto _nth = [](auto t) { return std::get<N>(t); };
+constexpr auto _fst = [](auto t) { return std::get<0>(t); };
+constexpr auto _snd = [](auto t) { return std::get<1>(t); };
 
-auto _rshift_  = [](auto x, auto y) { return x >> y; };
-auto _bit_and_ = std::bit_and{};
-auto _bit_xor_ = std::bit_xor{};
+constexpr auto _rshift_  = [](auto x, auto y) { return x >> y; };
+constexpr auto _bit_and_ = std::bit_and{};
+constexpr auto _bit_xor_ = std::bit_xor{};
 
-auto _odd  = [](auto x) { return (x % 2) == 1; };
-auto _even = [](auto x) { return (x % 2) == 0; };
+constexpr auto _odd  = [](auto x) { return (x % 2) == 1; };
+constexpr auto _even = [](auto x) { return (x % 2) == 0; };
 
 }  // namespace combinators
